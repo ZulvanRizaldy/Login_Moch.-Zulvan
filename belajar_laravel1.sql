8ea2235f3a5de9ad80108a5b59c1581fe36cc411
ZulvanRizaldy <75623089+ZulvanRizaldy@users.noreply.github.com>
2021-09-24 09:43:52 +0700

    Add files via upload

---
+++ belajar_laravel1.sql
@@ -0,0 +1,279 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 24, 2021 at 04:43 AM
+-- Server version: 10.4.20-MariaDB
+-- PHP Version: 8.0.8
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `belajar_laravel1`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `buku`
+--
+
+CREATE TABLE `buku` (
+  `id_buku` int(11) NOT NULL,
+  `judul_buku` varchar(100) NOT NULL,
+  `penulis` varchar(100) NOT NULL,
+  `penerbit` varchar(100) NOT NULL,
+  `deskripsi` varchar(200) NOT NULL,
+  `foto` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `buku`
+--
+
+INSERT INTO `buku` (`id_buku`, `judul_buku`, `penulis`, `penerbit`, `deskripsi`, `foto`) VALUES
+(4, 'SPY X FAMILY', 'Tatsuya Endo', 'Elex Media Komputindo', '', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `detail_peminjaman_buku`
+--
+
+CREATE TABLE `detail_peminjaman_buku` (
+  `id_detail_peminjaman_buku` int(11) NOT NULL,
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `id_buku` int(11) NOT NULL,
+  `qty` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `detail_peminjaman_buku`
+--
+
+INSERT INTO `detail_peminjaman_buku` (`id_detail_peminjaman_buku`, `id_peminjaman_buku`, `id_buku`, `qty`) VALUES
+(2, 2, 4, 1),
+(3, 3, 4, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(6) NOT NULL,
+  `nama_kelas` varchar(25) NOT NULL,
+  `kelompok` varchar(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kelompok`) VALUES
+(7, 'XIRPL-1', 'NODE JS'),
+(8, 'XIRPL-2', 'LARAVEL VUE'),
+(9, 'XIRPL-3', 'ANDROID'),
+(10, 'XIRPL-4', 'NODE JS'),
+(11, 'XIRPL-5', 'LARAVEL VUE'),
+(12, 'XIRPL-6', 'NODE JS'),
+(16, 'XIRPL-7', 'ANDROID');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `peminjaman_buku`
+--
+
+CREATE TABLE `peminjaman_buku` (
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `id_siswa` int(11) NOT NULL,
+  `tanggal_pinjam` date NOT NULL,
+  `tanggal_kembali` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `peminjaman_buku`
+--
+
+INSERT INTO `peminjaman_buku` (`id_peminjaman_buku`, `id_siswa`, `tanggal_pinjam`, `tanggal_kembali`) VALUES
+(1, 19, '2021-09-21', '2021-09-26'),
+(2, 19, '2021-09-21', '2021-09-26'),
+(3, 19, '2021-09-21', '2021-09-26');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pengembalian_buku`
+--
+
+CREATE TABLE `pengembalian_buku` (
+  `id_pengembalian_buku` int(11) NOT NULL,
+  `id_peminjaman_buku` int(11) NOT NULL,
+  `tanggal_pengembalian` date NOT NULL,
+  `denda` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `pengembalian_buku`
+--
+
+INSERT INTO `pengembalian_buku` (`id_pengembalian_buku`, `id_peminjaman_buku`, `tanggal_pengembalian`, `denda`) VALUES
+(1, 1, '2021-09-21', 0),
+(2, 2, '2021-09-21', 0),
+(3, 3, '2021-09-21', 0);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `id_siswa` int(3) NOT NULL,
+  `nama_siswa` text NOT NULL,
+  `tanggal_lahir` date NOT NULL,
+  `alamat` text NOT NULL,
+  `gender` enum('L','P','','') NOT NULL,
+  `username` varchar(15) NOT NULL,
+  `password` varchar(500) NOT NULL,
+  `id_kelas` int(6) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`id_siswa`, `nama_siswa`, `tanggal_lahir`, `alamat`, `gender`, `username`, `password`, `id_kelas`) VALUES
+(17, 'Zulpun', '2021-09-09', 'hatimu', 'L', 'mozu.ra', '827ccb0eea8a706', 11),
+(18, 'Po', '2021-09-14', 'Tulungagung', 'L', 'popo', '827ccb0eea8a706', 11),
+(19, 'co', '2021-09-14', 'Tulungagung', 'L', 'coco', '2c42e5cf1cdbafea04ed267018ef1511', 11);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `buku`
+--
+ALTER TABLE `buku`
+  ADD PRIMARY KEY (`id_buku`);
+
+--
+-- Indexes for table `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  ADD PRIMARY KEY (`id_detail_peminjaman_buku`),
+  ADD KEY `id_peminjaman_buku` (`id_peminjaman_buku`),
+  ADD KEY `id_buku` (`id_buku`);
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  ADD PRIMARY KEY (`id_peminjaman_buku`),
+  ADD KEY `id_siswa` (`id_siswa`);
+
+--
+-- Indexes for table `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  ADD PRIMARY KEY (`id_pengembalian_buku`),
+  ADD KEY `id_peminjaman_buku` (`id_peminjaman_buku`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`id_siswa`),
+  ADD KEY `id_kelas` (`id_kelas`),
+  ADD KEY `id_kelas_2` (`id_kelas`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `buku`
+--
+ALTER TABLE `buku`
+  MODIFY `id_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  MODIFY `id_detail_peminjaman_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(6) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  MODIFY `id_peminjaman_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  MODIFY `id_pengembalian_buku` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `siswa`
+--
+ALTER TABLE `siswa`
+  MODIFY `id_siswa` int(3) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `detail_peminjaman_buku`
+--
+ALTER TABLE `detail_peminjaman_buku`
+  ADD CONSTRAINT `detail_peminjaman_buku_ibfk_1` FOREIGN KEY (`id_buku`) REFERENCES `buku` (`id_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `peminjaman_buku`
+--
+ALTER TABLE `peminjaman_buku`
+  ADD CONSTRAINT `peminjaman_buku_ibfk_1` FOREIGN KEY (`id_siswa`) REFERENCES `siswa` (`id_siswa`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `pengembalian_buku`
+--
+ALTER TABLE `pengembalian_buku`
+  ADD CONSTRAINT `pengembalian_buku_ibfk_1` FOREIGN KEY (`id_peminjaman_buku`) REFERENCES `peminjaman_buku` (`id_peminjaman_buku`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD CONSTRAINT `siswa_ibfk_1` FOREIGN KEY (`id_kelas`) REFERENCES `kelas` (`id_kelas`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
